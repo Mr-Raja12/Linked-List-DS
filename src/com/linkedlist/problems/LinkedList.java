5e86f2c2b9ac2b6474808a5cea085106d83c83be
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-11 16:19:09 +0530

    Added: Delete Last Element In the LinkedList

--- src/com/linkedlist/problems/LinkedList.java
+++ src/com/linkedlist/problems/LinkedList.java
@@ -2,10 +2,12 @@
 //Use Case 1 is to create a simple linked list of 56,30 and 70.
 //Use Case 2 is to create LinkedList by adding 30,56 to 70. Sequence is 56-->30-->70 but adding vice-versa.
 //Use Case 3 is to create a LinkedList by appending 30 and 70 to 56. Sequence is 56-->30-->70.
-//Use case 4 is to add 30 in between 56 and 70 in the linked list. Sequence is 56-->30-->70.
-//Use case 5 is to delete first element in the linked list. Given sequence is 56-->30-->70 and o/p will be 30-->70.
+//Use Case 4 is to add 30 in between 56 and 70 in the linked list. Sequence is 56-->30-->70.
+//Use Case 5 is to delete first element in the linked list. Given sequence is 56-->30-->70 and o/p will be 30-->70.
+//Use Case 6 is to delete last element in the linked list. Given sequence is 56-->30-->70 and o/p will be 56-->30.
 
 package com.linkedlist.problems;
+
 /**
  * 
  * @author Raja
@@ -14,112 +16,97 @@ package com.linkedlist.problems;
 
 public class LinkedList {
 
-    static class Node
-    {
-        int data;
-        Node next;
-        Node(int data)
-        {
-            this.data = data;
-            this.next = null;
-        }
-    }
-    Node head = null;
-    Node tail = null;
+	static class Node {
+		int data;
+		Node next;
 
-    /*
-     * Adding to linked list.
-     */
-    public void addToLinkedList(int data)
-    {
-        Node newNode = new Node(data);
-        if (head == null)
-        {
-            head = newNode;
-        }
-        else
-        {
-            tail.next = newNode;
-            newNode.next = null;
-        }
-        tail = newNode;
-    }
+		Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
 
-    /*
-     * Add the node in the middle.
-     */
-    public void addAfterParticularNode(int value)
-    {
-        Node current = head;
-        while (current != null)
-        {
-            if (!(current.data == 56))
-            {
-                current = current.next;
-            }
-            else if (current.data == 56)
-            {
-                break;
-            }
-        }
-        Node newNode = new Node(value);
-        Node temp = current.next;
-        current.next = newNode;
-        newNode.next = temp;
-    }
+	Node head = null;
+	Node tail = null;
 
-    /*
-     * Delete the first element in the linked list
-     */
-    public void pop()
-    {
-        if (head == null)
-        {
-            System.out.println("List is empty");
-            return;
-        }
-        else
-        {
-            if (head != tail)
-            {
-                head = head.next;
-            }
-            else
-            {
-                head = tail = null;
-            }
-        }
-    }
+	/*
+	 * Adding to linked list.
+	 */
+	public void addToLinkedList(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+		} else {
+			tail.next = newNode;
+			newNode.next = null;
+		}
+		tail = newNode;
+	}
 
-    /*
-     * Print LinkedList
-     */
-    public void printLinkedList()
-    {
-        Node current = head;
-        if (head == null)
-        {
-            System.out.println("LinkedList is empty");
-            return;
-        }
-        while (current != null)
-        {
-            System.out.print(current.data+"  ");
-            current = current.next;
-        }
-    }
+	/*
+	 * Add the node in the middle.
+	 */
+	public void addAfterParticularNode(int value) {
+		Node current = head;
+		while (current != null) {
+			if (!(current.data == 56)) {
+				current = current.next;
+			} else if (current.data == 56) {
+				break;
+			}
+		}
+		Node newNode = new Node(value);
+		Node temp = current.next;
+		current.next = newNode;
+		newNode.next = temp;
+	}
 
-    public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        System.out.println("Welcome to LinkedList program");
-        list.addToLinkedList(56);
-        list.addToLinkedList(70);
-        list.addAfterParticularNode(30);
-        System.out.println("Original Linked List");
-        list.printLinkedList();
-        list.pop();
-        System.out.println();
-        System.out.println("After removing first element from Linked List");
-        list.printLinkedList();
-    }
+	/*
+	 * Delete the last element in the linked list
+	 */
+	public void popLast() {
+		if (head == null) {
+			System.out.println("List is empty");
+		} else {
+			if (head != tail) {
+				Node current = head;
+				while (current.next.next != null) {
+					current = current.next;
+				}
+				tail = current;
+				tail.next = null;
+			} else {
+				head = tail = null;
+			}
+		}
+	}
+
+	/*
+	 * Print LinkedList
+	 */
+	public void printLinkedList() {
+		Node current = head;
+		if (head == null) {
+			System.out.println("LinkedList is empty");
+			return;
+		}
+		while (current != null) {
+			System.out.print(current.data + "  ");
+			current = current.next;
+		}
+	}
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		System.out.println("Welcome to LinkedList program");
+		list.addToLinkedList(56);
+		list.addToLinkedList(70);
+		list.addAfterParticularNode(30);
+		System.out.println("Original Linked List");
+		list.printLinkedList();
+		list.popLast();
+		System.out.println();
+		System.out.println("After removing last element from Linked List");
+		list.printLinkedList();
+	}
 }
